2fd59a453af614e07d082207c2a2df41296f1c80
joao gabriel <joaobiel2004@gmail.com>
2026-04-28 20:59:59 -0300

    amend! 1
    1

--- src/main/java/com/br/ifg/luziania/trabalho_p3/util/DBConnection.java
+++ src/main/java/com/br/ifg/luziania/trabalho_p3/util/DBConnection.java
@@ -1,17 +1,17 @@
 package com.br.ifg.luziania.trabalho_p3.util;
 
 import java.sql.Connection;
-import java.sql.Statement;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
-    private Connection connection;
-    private Statement statement;
-    private String usuario;
-    private String senha;
+    private static final String URL = "jdbc:postgresql://localhost:5432/locadora";
+    private static final String USUARIO = "postgres";
+    private static final String SENHA = "post1234";
 
+    private DBConnection() {}
 
-
-    public DBConnection() {}
-    //public static Connection getConexao() throws Exception {
-    //}
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USUARIO, SENHA);
+    }
 }
