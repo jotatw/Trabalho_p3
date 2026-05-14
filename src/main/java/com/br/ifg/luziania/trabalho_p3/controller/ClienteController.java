b1b7bae6dbd8fb8b1199a5524c174dfd76c329f4
joao gabriel <joaobiel2004@gmail.com>
2026-05-14 16:30:05 -0300

    Adiciona atualizacao de clientes pela tabela

--- src/main/java/com/br/ifg/luziania/trabalho_p3/controller/ClienteController.java
+++ src/main/java/com/br/ifg/luziania/trabalho_p3/controller/ClienteController.java
@@ -19,6 +19,7 @@ import java.util.List;
 
 public class ClienteController {
     private final ClienteService clienteService = new ClienteService();
+    private Cliente clienteSelecionado;
 
     @FXML private TextField campoNome;
     @FXML private TextField campoCpf;
@@ -26,6 +27,7 @@ public class ClienteController {
     @FXML private TextField campoEmail;
     @FXML private TextField campoCnh;
     @FXML private Button btnVoltar;
+    @FXML private Button btnAtualizar;
 
     @FXML private TableView <Cliente> tabelaCliente;
     @FXML private TableColumn <Cliente, String> colunaNome;
@@ -33,6 +35,30 @@ public class ClienteController {
     @FXML private TableColumn <Cliente, String> colunaTelefone;
     @FXML private TableColumn <Cliente, String> colunaEmail;
     @FXML private TableColumn <Cliente, String> colunaCnh;
+
+    @FXML
+    public void initialize() {
+        //aplica as mascaras no campo de entrada
+        MascaraUtil.cpf(campoCpf);
+        MascaraUtil.cnh(campoCnh);
+        MascaraUtil.telefone(campoTelefone);
+
+        //configura cada coluna para pegar o atributo certo do model
+        colunaNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
+        colunaCpf.setCellValueFactory(new PropertyValueFactory<>("cpf"));
+        colunaCnh.setCellValueFactory(new PropertyValueFactory<>("cnh"));
+        colunaTelefone.setCellValueFactory(new PropertyValueFactory<>("telefone"));
+        colunaEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+
+        carregarTabela();
+
+        tabelaCliente.getSelectionModel().selectedItemProperty().addListener((obs, antigo, selecionado) -> {
+            if (selecionado != null) {
+                clienteSelecionado = selecionado;
+                preecherFormulario(selecionado);
+            }
+        });
+    }
 
     @FXML
     private void salvar() {
@@ -97,6 +123,9 @@ public class ClienteController {
         campoCnh.clear();
         campoTelefone.clear();
         campoEmail.clear();
+
+        clienteSelecionado = null;
+        tabelaCliente.getSelectionModel().clearSelection();
     }
     @FXML
     private void voltar() {
@@ -113,21 +142,66 @@ public class ClienteController {
         }
     }
     @FXML
-    public void initialize() {
-        //aplica as mascaras no campo de entrada
-        MascaraUtil.cpf(campoCpf);
-        MascaraUtil.cnh(campoCnh);
-        MascaraUtil.telefone(campoTelefone);
+    private void atualizar() {
+        if (clienteSelecionado == null) {
+            mostraAlerta("Selecione um cliente para atualizar!");
+            return;
+        }
+        String nome = campoNome.getText().trim();
+        String cpf = campoCpf.getText().trim();
+        String cnh = campoCnh.getText().trim();
+        String telefone = campoTelefone.getText().trim();
+        String email = campoEmail.getText().trim();
 
-        //configura cada coluna para pegar o atributo certo do model
-        colunaNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
-        colunaCpf.setCellValueFactory(new PropertyValueFactory<>("cpf"));
-        colunaCnh.setCellValueFactory(new PropertyValueFactory<>("cnh"));
-        colunaTelefone.setCellValueFactory(new PropertyValueFactory<>("telefone"));
-        colunaEmail.setCellValueFactory(new PropertyValueFactory<>("email"));
+        if (ValidacaoUtil.campoVazio(nome)) {
+            mostraAlerta("Nome e obrigatorio!");
+            return;
+        }
+        if (!ValidacaoUtil.cpfValido(cpf)) {
+            mostraAlerta(("CPF invalido!  use o formato: 000.000.000-00"));
+            return;
+        }
+        if (!ValidacaoUtil.cnhValido(cnh)) {
+            mostraAlerta("CNH invalida! informe 11 digitos numericos");
+            return;
+        }
+        if (ValidacaoUtil.campoVazio(telefone)) {
+            mostraAlerta("Telefone e obrigatorio!");
+        }
+        if (!ValidacaoUtil.telefoneValido(telefone)) {
+            mostraAlerta("Telefone invalido! Use o formato: (00) 99999-9999 ou (00) 1111-2222");
+            return;
+        }
+        if (ValidacaoUtil.campoVazio(email)) {
+            mostraAlerta("Email e obrigatorio!");
+            return;
+        }
+        if (!ValidacaoUtil.emailValido(email)) {
+            mostraAlerta("Email invalido!");
+        }
+        try {
+            clienteSelecionado.setNome(nome);
+            clienteSelecionado.setCpf(cpf);
+            clienteSelecionado.setCnh(cnh);
+            clienteSelecionado.setTelefone(telefone);
+            clienteSelecionado.setEmail(email);
 
-        carregarTabela();
+            clienteService.atualizar(clienteSelecionado);
+            mostraSucesso("Cliente atualizado com sucesso!");
+            limpar();
+            carregarTabela();
+        } catch (SQLException e) {
+            mostraAlerta("Erro ao atualizar cliente. Verifique se CPF, CNH ou e-mail já estão cadastrados.");
+        }
     }
+    private void preecherFormulario(Cliente cliente) {
+        campoNome.setText(cliente.getNome());
+        campoCpf.setText(cliente.getCpf());
+        campoCnh.setText(cliente.getCnh());
+        campoTelefone.setText(cliente.getTelefone());
+        campoEmail.setText(cliente.getEmail());
+    }
+
     private void carregarTabela() {
         try {
             List<Cliente> lista = clienteService.listarTodos();
