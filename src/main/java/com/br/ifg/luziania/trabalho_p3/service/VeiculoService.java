b921cab4face867d3a5a23f94292a873adc7e59a
joão gabriel <129410759+jotatw@users.noreply.github.com>
2026-05-13 22:36:40 -0300

    Padroniza VeiculoService

--- src/main/java/com/br/ifg/luziania/trabalho_p3/service/VeiculoService.java
+++ src/main/java/com/br/ifg/luziania/trabalho_p3/service/VeiculoService.java
@@ -8,33 +8,55 @@ import java.sql.SQLException;
 import java.util.List;
 
 public class VeiculoService {
-    private VeiculoDAO veiculoDAO = new VeiculoDAO();
+
+    private final VeiculoDAO veiculoDAO = new VeiculoDAO();
 
     public void salvar(Veiculo veiculo) throws SQLException {
         veiculo.setDisponivel(true);
         veiculoDAO.salvar(veiculo);
-        LogUtil.registrarAcao("VEICULO_CADASTRADO", "PLACA=" + veiculo.getPlaca());
+
+        LogUtil.registrarAcao(
+                "VEICULO_CADASTRADO",
+                "PLACA=" + veiculo.getPlaca()
+        );
     }
+
     public List<Veiculo> listarTodos() throws SQLException {
         return veiculoDAO.listarTodos();
     }
+
     public List<Veiculo> listarDisponiveis() throws SQLException {
         return veiculoDAO.listarDisponiveis();
     }
+
     public Veiculo buscarPorPlaca(String placa) throws SQLException {
         return veiculoDAO.buscarPorPlaca(placa);
     }
+
     public void atualizar(Veiculo veiculo) throws SQLException {
         veiculoDAO.atualizar(veiculo);
-        LogUtil.registrarAcao("VEICULO_ATUALIZADO", "ID=" + veiculo.getId() + ", PLACA=" + veiculo.getPlaca());
+
+        LogUtil.registrarAcao(
+                "VEICULO_ATUALIZADO",
+                "ID=" + veiculo.getId() + ", PLACA=" + veiculo.getPlaca()
+        );
     }
+
     public void atualizarDisponivel(boolean disponivel, int id) throws SQLException {
         veiculoDAO.atualizarDisponivel(disponivel, id);
 
-        LogUtil.registrarAcao("VEICULO_DISPONIBILIDADE_ATUALIZADA", "ID=" + id + ", DISPONIVEL=" + disponivel);
+        LogUtil.registrarAcao(
+                "VEICULO_DISPONIBILIDADE_ATUALIZADA",
+                "ID=" + id + ", DISPONIVEL=" + disponivel
+        );
     }
+
     public void deletar(int id) throws SQLException {
         veiculoDAO.deletar(id);
-        LogUtil.registrarAcao("VEICULO_DELETADO", "ID=" + id);
+
+        LogUtil.registrarAcao(
+                "VEICULO_DELETADO",
+                "ID=" + id
+        );
     }
 }
